e9f95593e8262284099d29d13e2d95f8a12f3eb8
lIlIlIIIlIIl <edi_park@naver.com>
2025-09-22 14:20:58 +0900

    squat motion implementing

--- squat.py
+++ squat.py
@@ -1,5 +1,6 @@
 import dynamixel_sdk as dxl
 import init_pose
+import time
 
 # pose init
 init_pose.init_pose()
@@ -30,6 +31,8 @@ DOF_POSITION = {
 ADDR_TORQUE_ENABLE = 24
 ADDR_GOAL_POSITION = 30 
 ADDR_MOVING_SPEED = 32
+ADDR_PRESENT_POSITION = 36  
+
 
 LEN_1BYTE = 1
 LEN_2BYTE = 2
@@ -39,6 +42,20 @@ packetHandler = dxl.PacketHandler(PROTOCOL_VERSION)
 
 portHandler.openPort()
 portHandler.setBaudRate(BAUDRATE)
+
+
+
+def get_present_position(motor_id):
+    pos, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(
+        portHandler, motor_id, ADDR_PRESENT_POSITION
+    )
+    if dxl_comm_result != dxl.COMM_SUCCESS:
+        print(f"모터 {motor_id} 현재 위치 읽기 오류: {packetHandler.getTxRxResult(dxl_comm_result)}")
+        return 512  # 오류 시 기본값
+    elif dxl_error != 0:
+        print(f"모터 {motor_id} 에러: {packetHandler.getRxPacketError(dxl_error)}")
+        return 512
+    return pos
 
 
 def close_port():
@@ -72,8 +89,8 @@ def move_motors_slow(motor_ids, goal_positions, moving_speed):
     """
     # 속도 동시 설정
     group_speed = dxl.GroupSyncWrite(portHandler, packetHandler, ADDR_MOVING_SPEED, LEN_2BYTE)
-    for motor_id in motor_ids:
-        speed = max(1, min(1023, int(moving_speed)))
+    for i, motor_id in enumerate(motor_ids):
+        speed = max(1, min(1023, int(moving_speed[i])))
         param_speed = [speed & 0xFF, (speed >> 8) & 0xFF]
         if not group_speed.addParam(motor_id, param_speed):
             print(f"속도 파라미터 추가 실패: ID {motor_id}")
@@ -100,10 +117,18 @@ if __name__ == "__main__":
     target_ids = [3, 4, 5, 9, 10, 11]
     if enable_torque_for(target_ids):
         move_motors_slow(
-            motor_ids=target_ids,
-            goal_positions={3: 444, 4: 204, 5: 745, 9: 580, 10: 820, 11: 279},
-            moving_speed=500,
-        )
+                motor_ids=target_ids,
+                goal_positions={3: 444, 4: 204, 5: 745, 9: 580, 10: 820, 11: 279},
+                moving_speed=[50 * (36/233), 50 * (206/233), 50, 50 * (36/233), 50 * (206/233), 50],
+            )
+        time.sleep(7)  
+        move_motors_slow(
+                motor_ids=target_ids,
+                goal_positions={3: DOF_POSITION[3], 4: DOF_POSITION[4], 5: DOF_POSITION[5], 
+                                9: DOF_POSITION[9], 10: DOF_POSITION[10], 11: DOF_POSITION[11]},
+                moving_speed=[50 * (36/233), 50 * (206/233), 50, 50 * (36/233), 50 * (206/233), 50],
+            )
+        
         
     close_port()
 
